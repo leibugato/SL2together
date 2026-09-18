c3b2c6c40217ec84a4c69f2a4d9bdbd9161a6c5b
leibugato <leibugato@users.noreply.github.com>
2026-09-19 00:24:48 +0800

    fix: default mod builder port to 80

--- mod-builder/service.py
+++ mod-builder/service.py
@@ -152,6 +152,6 @@ if __name__ == "__main__":
     uvicorn.run(
         app,
         host="0.0.0.0",
-        port=int(os.environ.get("PORT", "8080")),
+        port=int(os.environ.get("PORT", "80")),
         log_level="info",
     )
